c46840722db3905db7cf90ab003fbfc79f5d0b9c
Dimitil <boikovdmitrii@gmail.com>
2020-12-05 14:35:29 +0300

    completed

---
+++ scaleApp_ru.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>ScaleApp</source>
+        <translation>Приложение масштабирования</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="25"/>
+        <source>&amp;SliderLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="53"/>
+        <source>Scale %</source>
+        <translation>Масштаб</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="72"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="99"/>
+        <source>&amp;SpinLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="135"/>
+        <source>&amp;EditLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="170"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="179"/>
+        <source>do nothing</source>
+        <translation>Ничего не делается</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="184"/>
+        <source>toolBar</source>
+        <translation>Панель инструментов</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="203"/>
+        <source>&amp;Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="206"/>
+        <source>Ctrl+E</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="211"/>
+        <source>About</source>
+        <translation>О приложении</translation>
+    </message>
+</context>
+</TS>
